ebee0775356c31dbc441be4945e32617bef33f58
eileen <zwleileen@gmail.com>
2025-02-19 15:12:19 +0800

    show values

--- src/components/FriendProfile/FriendProfile.jsx
+++ src/components/FriendProfile/FriendProfile.jsx
@@ -1,11 +1,38 @@
-const FriendProfile = () => {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import * as valuesService from '../../services/valuesService';
 
-  //req params id from URL
-  //userService
+const FriendProfile = () => {
+  const { friendId } = useParams();
+  const [ friendValues, setFriendValues ] = useState();
+
+  useEffect(() => {
+      const fetchValues = async () => {
+        try {
+          if (friendId) {
+            const fetchedValues = await valuesService.show(friendId);
+            console.log(fetchedValues);
+            setFriendValues(fetchedValues);
+          }
+        } catch (err) {
+          console.log("Error fetching values:", err);
+        }
+      };
+  
+      fetchValues();
+    }, [friendId]);
 
     return (
       <main>
-        Alice's Profile!
+        {friendId}'s Profile!
+        <div>
+        <h1>Values for User {friendId}</h1>
+        {friendValues ? (
+        <pre>{JSON.stringify(friendValues, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
       </main>
     );
   };
