5e4966ed91c0bfd77f6f895f77974b3515602207
Amos Yeo <amosyeo.sh@gmail.com>
2025-02-20 17:45:06 +0800

    created 2x components FriendRequest and FriendShow. This is for use in Dashboard, to toggle  between Friends and Friend Request. Note temp data in Friend Request component.

---
+++ src/components/FriendRequest/FriendRequest.jsx
@@ -0,0 +1,13 @@
+const FriendRequest = ({ user }) => {
+  console.log(user);
+  return (
+    <>
+      <h2>Friend Requests</h2>
+      {/* <p>{user}</p> */}
+      <p>this is Friend Request Component site</p>
+      <p>toggling friend request</p>
+    </>
+  );
+};
+
+export default FriendRequest;
