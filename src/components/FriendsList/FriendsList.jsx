91f9207855eabc50e4a768bde8acaef6214596f4
eileen <zwleileen@gmail.com>
2025-02-21 16:45:56 +0800

    filter based on no. of matched values

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 const FriendsList = ({ users, valuesResults }) => {
   const { user } = useContext(UserContext);
   const [ matches, setMatches ] = useState([]);
+  const [ exactMatches, setExactMatches ] = useState(0);
+  const [ showMatches, setShowMatches] = useState([]);
 
   useEffect(() => {
     const fetchMatches = async () => {
@@ -13,6 +15,7 @@ const FriendsList = ({ users, valuesResults }) => {
         if (!user) return;
         const matchedUsers = await valuesService.getMatches();
         console.log(matchedUsers);
+        setShowMatches(matchedUsers);
         setMatches(matchedUsers);
       } catch (error) {
         console.error("Error finding matches:", error);
@@ -21,10 +24,26 @@ const FriendsList = ({ users, valuesResults }) => {
     fetchMatches();
   }, [user]);
 
+  useEffect(() => {
+    const filteredMatches = showMatches.filter(match => match.matchedValues === exactMatches);
+    setMatches(filteredMatches);
+  }, [exactMatches, showMatches]);
+
 
   return (
     <main>
       <h1>People with values that matched yours</h1>
+
+      <div>
+        <label htmlFor="exactMatches">No. of matching values: </label>
+        <select id="exactMatches" value={exactMatches} onChange={(e)=>setExactMatches(Number(e.target.value))}>
+          <option value={0}>Show all matches</option>
+          <option value={1}>1 match</option>
+          <option value={2}>2 matches</option>
+          <option value={3}>3 matches</option>
+        </select>
+      </div>
+
       {matches.length > 0 ? (
         matches.map((match) => (
           <div key={match.user._id}>
@@ -43,7 +62,7 @@ const FriendsList = ({ users, valuesResults }) => {
           </div>
         ))
       ) : (
-        <p>No matches found.</p>
+        <p>No matches found. Try changing the no. of matching values.</p>
       )}
     </main>
   );
