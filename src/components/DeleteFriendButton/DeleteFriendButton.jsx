05d66c89a0a9f43189f1c0d7bcbb2a8fd14997a4
eileen <zwleileen@gmail.com>
2025-02-24 16:37:46 +0800

    edited deletefriend

--- src/components/DeleteFriendButton/DeleteFriendButton.jsx
+++ src/components/DeleteFriendButton/DeleteFriendButton.jsx
@@ -1,11 +1,17 @@
+import { useNavigate } from "react-router";
 import friendsService from "../../services/friendsService";
 import { Button } from "@mui/material";
 
 const DeleteFriendButton = ({ userIdToDelete }) => {
   const handleDeleteButton = async (userIdToDelete) => {
+    const navigate = useNavigate();
+    try{
     await friendsService.deleteFriend(userIdToDelete);
-    window.location.reload();
+    navigate("/users");
     //   possibly use navigate(`/users/${userIdToDelete}`); together with state change for react to detect and reload
+    } catch (error) {
+      console.error("Error deleting friend:", error);
+    }
   };
 
   return (
