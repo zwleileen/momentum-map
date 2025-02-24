6a3de287388f0f1d34672777e633b4fb7bd90ee2
eileen <zwleileen@gmail.com>
2025-02-24 12:53:46 +0800

    managed to show mereceived messages

---
+++ src/components/MessagesList/MessagesList.jsx
@@ -0,0 +1,51 @@
+import { useContext, useEffect, useState } from "react";
+import * as messageService from '../../services/messageService';
+import { UserContext } from "../../contexts/UserContext";
+
+
+const MessagesList = () => {
+    const { user } = useContext(UserContext);
+    const [ messages, setMessages ] = useState();
+
+      useEffect(() => {
+        const fetchMessages = async () => {
+          try {
+            if (user) {
+              const fetchedMessages = await messageService.show(user._id);
+    
+              if (!fetchedMessages) {
+                console.log("No messages to show");  // Debug log
+                return;
+              }
+              console.log(fetchedMessages);
+              setMessages(fetchedMessages);
+            }
+          } catch (err) {
+            console.log("Error fetching messages:", err);
+          }
+        };
+    
+        fetchMessages();
+      }, [user]);
+
+
+    return (
+        <>
+        <h2>Messages from friends</h2>
+        {messages.length > 0 ? (
+            <ul>
+                {messages.map((msg) => (
+                    <li key={msg._id}>
+                        <strong>{msg.sender.username}: </strong>
+                        {msg.text}
+                    </li>
+                ))}
+            </ul>
+        ) : (
+            <p>No message to show</p>
+        )}
+        </>
+    )
+}
+
+export default MessagesList
