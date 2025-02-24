b2056015635214390cd2978223af44a28fd0ed93
eileen <zwleileen@gmail.com>
2025-02-24 12:34:09 +0800

    managed to send message to backend

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import * as messageService from '../../services/messageService';
+import { useParams } from 'react-router';
+import { UserContext } from '../../contexts/UserContext';
 
 const Message = () => {
+    const {friendId} = useParams(); //receiver Id
+    const {user} = useContext(UserContext); //sender Id
     const [formData, setFormData] = useState({ text: '' });
     
     const handleChange = (evt) => {
@@ -10,14 +14,21 @@ const Message = () => {
     
     const handleSubmit = async (evt) => {
         evt.preventDefault();
-        try {
-            const newMessage = await messageService.create(formData);
+        const message = {
+            text: formData.text,
+            sender: user._id,        // sender ID from logged-in user
+            receiver: friendId,      // receiver ID from route parameter
+          };
+      
+          try {
+            const newMessage = await messageService.create(message);
             console.log("Message created:", newMessage);
             setFormData({ text: '' });
           } catch (error) {
             console.error("Error:", error.message);
           }
-      };
+        };
+      
 
     return (
         <>
